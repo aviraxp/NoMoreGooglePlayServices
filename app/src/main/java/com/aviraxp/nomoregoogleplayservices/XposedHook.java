2cce05a9cb150fd9c987fd95bc08e5c41d6b1aaa
Wang Han <416810799@qq.com>
2017-02-08 15:11:59 +0800

    XposedHook: Support Stock Google Applications
    Stock Google applications use another method to record it.
    
    Add them.

--- app/src/main/java/com/aviraxp/nomoregoogleplayservices/XposedHook.java
+++ app/src/main/java/com/aviraxp/nomoregoogleplayservices/XposedHook.java
@@ -13,19 +13,35 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedHook implements IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         try {
-            Class<?> clazz1 = XposedHelpers.findClass("com.google.android.gms.common.GoogleApiAvailability", lpparam.classLoader);
-            Method[] methods1 = XposedHelpers.findMethodsByExactParameters(clazz1, int.class, Context.class);
-            XposedHelpers.findAndHookMethod(clazz1, methods1[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
-            XposedBridge.log("Hook GoogleApiAvailability Check Success" + lpparam.packageName + ": " + clazz1 + "." + methods1[0].getName());
-        } catch (XposedHelpers.ClassNotFoundError ignored) {
+            Class<?> clazz = XposedHelpers.findClass("com.google.android.gms.common.GoogleApiAvailability", lpparam.classLoader);
+            Method[] methods = XposedHelpers.findMethodsByExactParameters(clazz, int.class, Context.class);
+            XposedHelpers.findAndHookMethod(clazz, methods[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
+            XposedBridge.log("Hook GoogleApiAvailability Check Success: " + lpparam.packageName + ": " + String.valueOf(clazz) + "." + methods[0].getName());
+        } catch (XposedHelpers.ClassNotFoundError | IndexOutOfBoundsException ignored) {
         }
 
         try {
-            Class<?> clazz2 = XposedHelpers.findClass("com.google.android.gms.common.GoogleServicesUtil", lpparam.classLoader);
-            Method[] methods2 = XposedHelpers.findMethodsByExactParameters(clazz2, int.class, Context.class);
-            XposedHelpers.findAndHookMethod(clazz2, methods2[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
-            XposedBridge.log("Hook Deprecated GoogleApiAvailability Check Success" + lpparam.packageName + ": " + clazz2 + "." + methods2[0].getName());
-        } catch (XposedHelpers.ClassNotFoundError ignored) {
+            Class<?> clazz = XposedHelpers.findClass("com.google.android.gms.common.GoogleServicesUtil", lpparam.classLoader);
+            Method[] methods = XposedHelpers.findMethodsByExactParameters(clazz, int.class, Context.class);
+            XposedHelpers.findAndHookMethod(clazz, methods[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
+            XposedBridge.log("Hook Deprecated GoogleApiAvailability Check Success: " + lpparam.packageName + ": " + String.valueOf(clazz) + "." + methods[0].getName());
+        } catch (XposedHelpers.ClassNotFoundError | IndexOutOfBoundsException ignored) {
+        }
+
+        try {
+            Class<?> clazz = XposedHelpers.findClass("com.google.android.gms.common.GoogleApiAvailabilityLight", lpparam.classLoader);
+            Method[] methods = XposedHelpers.findMethodsByExactParameters(clazz, int.class, Context.class);
+            XposedHelpers.findAndHookMethod(clazz, methods[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
+            XposedBridge.log("Hook GoogleApiAvailability Check Success: " + lpparam.packageName + ": " + String.valueOf(clazz) + "." + methods[0].getName());
+        } catch (XposedHelpers.ClassNotFoundError | IndexOutOfBoundsException ignored) {
+        }
+
+        try {
+            Class<?> clazz = XposedHelpers.findClass("com.google.android.gms.common.GoogleServicesUtilLight", lpparam.classLoader);
+            Method[] methods = XposedHelpers.findMethodsByExactParameters(clazz, int.class, Context.class);
+            XposedHelpers.findAndHookMethod(clazz, methods[0].getName(), Context.class, XC_MethodReplacement.returnConstant(0));
+            XposedBridge.log("Hook Deprecated GoogleApiAvailability Check Success: " + lpparam.packageName + ": " + String.valueOf(clazz) + "." + methods[0].getName());
+        } catch (XposedHelpers.ClassNotFoundError | IndexOutOfBoundsException ignored) {
         }
     }
 }
